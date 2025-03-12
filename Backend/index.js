efa0cdfa6eaf044faa1911c1e5e31620c659429e
YadavPrasad <yadavprasad.exe@.gmail.com>
2025-03-12 12:48:54 +0530

    Login

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const PORT = 3000;
+const PORT = 3002;
 
 mdb.connect("mongodb+srv://yadav:yadav@credential.25wb3.mongodb.net/Credential?retryWrites=true&w=majority&appName=Credential")
    .then(() => {
@@ -44,9 +44,32 @@ app.post("/signup", async (req, res) => {
 
 
 app.post("/login", async(req, res) => {
+  const{Username, password} = req.body;
   try{
-    const{Username, password} = res.body;
-    const user = await 
+    console.log(Username+" "+password);
+    const userDetails = await Signup.findOne({Username});
+
+    if(userDetails) {
+      //alert("signup please");
+      const storePw = userDetails.password;
+      bcrypt.compare(password, storePw, (err, res) => {
+        if(err) {
+          console.log(err);
+        }
+        if(res) {
+          console.log("passwords match !!");
+        }
+        else console.log("passwords doesn't match !!");
+      });
+      res.status(200).json({message : "hi"});
+    }
+    else{
+      return res.status(200).json({message:"Otha veliya poda poolu..."});
+    }
+  }
+  catch(err){
+    console.log(err);
+    return res.status(200).json({message:"What are you doing step bro take it out..."});
   }
 }) 
 
